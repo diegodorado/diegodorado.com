d9447c2792d63372eef44f483538d5f346352053
Diego Dorado <diegodorado@gmail.com>
2025-06-01 17:55:42 -0300

    new theme

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,62 +1,42 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Link from '../link'
 import Brand from './brand'
 import LanguagesLinks from './languages-links'
 import SocialLinks from './social-links'
 import { useTranslation } from 'react-i18next'
-import Context from '../context'
+import { useLocation } from "@reach/router"
 
-const meta = [
-  {
-    name: `viewport`,
-    content: `width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no`,
-  },
-  {
-    name: `theme-color`,
-    content: `#ff0000`,
-  },
-  {
-    name: `msapplication-navbutton-color`,
-    content: `#ff0000`,
-  },
-  {
-    name: `apple-mobile-web-app-status-bar-style`,
-    content: `#ff0000`,
-  },
-]
+const PLink = ({ to, className, ...rest }) => {
+  const location = useLocation()
+  const isPartiallyActive = location.pathname.includes(to)
+  return (
+    <Link to={to} className={`${className} ${isPartiallyActive ? "active" : ""}`} {...rest} />
+  )
+}
 
-const partiallyActive =
-  (className) =>
-  ({ isPartiallyCurrent }) => ({
-    className: `${className ? className : ''} ${isPartiallyCurrent ? 'active' : ''}`,
-  })
-
-const PLink = ({ className, ...rest }) => (
-  <Link getProps={partiallyActive(className)} {...rest} />
-)
-
-const Header = ({ location, bodyClass = '' }) => {
+const Header = ({ location }) => {
   const [t] = useTranslation()
-  const { state } = useContext(Context)
 
-  // <Helmet bodyAttributes={{class: `${state.theme} ${bodyClass}` }}  />
   return (
     <header>
       <Brand title="diego dorado" />
-      <nav>
-        <SocialLinks />
-        <LanguagesLinks location={location} />
-        <br />
-        <PLink to={`/work`}>{t('Work')}</PLink>|
-        <PLink to={`/music`}>{t('Music')}</PLink>|
-        <PLink className="labs" to={`/labs`}>
-          <i>L</i>
-          <i>a</i>
-          <i>b</i>
-          <i>s</i>
-        </PLink>
-        |<PLink to={`/bio`}>{t('Bio')}</PLink>
-      </nav>
+      <div className="nav">
+        <nav>
+          <SocialLinks />
+          <LanguagesLinks location={location} />
+        </nav>
+        <nav className="main">
+          <PLink to={`/work`}>{t('Work')}</PLink>|
+          <PLink to={`/music`}>{t('Music')}</PLink>|
+          <PLink className="labs" to={`/labs`}>
+            <i>L</i>
+            <i>a</i>
+            <i>b</i>
+            <i>s</i>
+          </PLink>
+          |<PLink to={`/bio`}>{t('Bio')}</PLink>
+        </nav>
+      </div>
     </header>
   )
 }
